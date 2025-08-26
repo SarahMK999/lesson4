2b221e950cae9710e4f93964658b69a5c6f2cff1
SarahMK999 <sarahmimo999@gmail.com>
2025-08-26 09:38:02 +0300

    try

---
+++ struckts/target/debug/deps/struckts-7a2860babc8a06d9.d
@@ -0,0 +1,9 @@
+C:\Users\sarah\OneDrive\שולחן העבודה\RUST PROGRAMING\lesson\struckts\target\debug\deps\struckts-7a2860babc8a06d9.d: src\main.rs Cargo.toml
+
+C:\Users\sarah\OneDrive\שולחן העבודה\RUST PROGRAMING\lesson\struckts\target\debug\deps\libstruckts-7a2860babc8a06d9.rmeta: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ struckts/target/debug/deps/struckts-eb299b89a9701904.d
@@ -0,0 +1,9 @@
+C:\Users\sarah\OneDrive\שולחן העבודה\RUST PROGRAMING\lesson\struckts\target\debug\deps\struckts-eb299b89a9701904.d: src\main.rs Cargo.toml
+
+C:\Users\sarah\OneDrive\שולחן העבודה\RUST PROGRAMING\lesson\struckts\target\debug\deps\libstruckts-eb299b89a9701904.rmeta: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
